d20b1feabeb07580b116895494084bc3a76ed10d
Hezza <57487993+you-say-that@users.noreply.github.com>
2021-06-28 09:58:33 +0100

    Update collect_sheet_files.py

--- collect_sheet_files.py
+++ collect_sheet_files.py
@@ -1,6 +1,6 @@
 get_ipython().system('pip install --quiet PyDrive')
 from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive'
+from pydrive.drive import GoogleDrive
 gauth = GoogleAuth()
 drive = GoogleDrive(gauth)
 
